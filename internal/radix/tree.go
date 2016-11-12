0de35dbe91375f0747bce5aef33e8ead4c7acf32
Fan Yang <yangfan879@gmail.com>
2016-11-12 21:00:26 +0800

    [WIP] radix: initial implementation

--- internal/radix/tree.go
+++ internal/radix/tree.go
@@ -1,31 +1,36 @@
 package radix
 
 import (
-	"bytes"
 	"fmt"
+	"net/http"
 	"strings"
 )
 
-type NodeType int
+//go:generate stringer -type=nodeType
+
+type nodeType int
+
+func (i nodeType) IsNotNil() bool { return i&nonNilNode != 0 }
 
 const (
-	TypeNil   NodeType = 0
-	TypeEmpty NodeType = 1 << iota
-	TypeSimple
-	TypeMatch
-	TypeCapture
-	TypeHasValue = TypeSimple | TypeMatch | TypeCapture
+	nonNilNode nodeType = 1 << iota
+	staticNode
+	capDirNode
+	capAllNode
+	nodeTypeMask = staticNode | capDirNode | capAllNode
 )
 
-type Node struct {
-	dir   string
-	child []Node
-	index []byte
-	typ   NodeType
-	v     int
+type Data struct {
+	Handler http.Handler
 }
 
-const None = -1
+type Node struct {
+	dir      string
+	typ      nodeType
+	index    string
+	children []Node
+	Data
+}
 
 type Tree struct {
 	root Node
@@ -39,79 +44,78 @@ func byteForIdx(dir string) byte {
 }
 
 func (node *Node) append(c Node) *Node {
-	node.child = append(node.child, c)
-	node.index = append(node.index, byteForIdx(c.dir))
-	return &node.child[len(node.child)-1]
+	node.children = append(node.children, c)
+	node.index = node.index + string(byteForIdx(c.dir))
+	return &node.children[len(node.children)-1]
 }
 
-func (t *Tree) Add(path string, v int) (ov int, replace bool) {
-	if t.root.typ == TypeNil {
-		t.root.dir = ""
-		t.root.typ = TypeEmpty
-		t.root.v = None
-	}
+func (t *Tree) Add(path string, v Data) (old Data, replace bool) {
 	ss := strings.Split(path, "/")
 	return t.root.insert(ss[1:], v)
 }
 
-func (node *Node) insert(ss []string, v int) (ov int, replace bool) {
+func (node *Node) insert(ss []string, v Data) (old Data, replace bool) {
 	if len(ss) == 0 {
-		ov, replace = node.v, node.typ&TypeHasValue != 0
-		node.v = v
-		node.typ = TypeSimple
-		return ov, replace
+		old, replace = node.Data, node.typ.IsNotNil()
+		node.Data = v
+		node.typ = staticNode | nonNilNode
+		return old, replace
 	}
 	var (
-		dir   = ss[0]
-		b     = byteForIdx(dir)
-		index = node.index
-		i     = bytes.IndexByte(index, b)
+		dir      = ss[0]
+		b        = byteForIdx(dir)
+		index    = node.index
+		i        = strings.IndexByte(index, b)
+		children = node.children
 	)
-	for ; i >= 0; i = bytes.IndexByte(index, b) {
-		if next := &node.child[i]; next.dir == dir {
+	for ; i >= 0; i = strings.IndexByte(index, b) {
+		if next := &children[i]; next.dir == dir {
 			return next.insert(ss[1:], v)
 		}
-		index = index[i+1:]
+		index, children = index[i+1:], children[i+1:]
 	}
 	next := node.append(Node{
 		dir: dir,
-		typ: TypeEmpty,
-		v:   None,
+		typ: staticNode,
 	})
 	return next.insert(ss[1:], v)
 }
 
-func (t *Tree) Lookup(path string) (v int, exact bool) {
-	if t.root.typ == TypeNil {
-		return None, false
-	}
-
+func (t *Tree) Lookup(path string) *Node {
 	node := &t.root
 
 OUTER:
-	for ptr := 0; len(path) > 0; path, ptr = path[ptr+1:], 0 {
-		fmt.Printf("node{dir: %q, #child: %d, index: %s, type: %d}\n", node.dir, len(node.child), string(node.index), node.typ)
-		if ptr = strings.IndexByte(path, '/'); ptr < 0 {
-			// TODO
-			ptr = len(path)
+	for pos := 0; len(path) > 0; path = path[pos:] {
+		path = path[1:]
+
+		if pos = strings.IndexByte(path, '/'); pos < 0 {
+			pos = len(path)
 		}
 
 		var (
-			dir   = path[:ptr]
-			b     = byteForIdx(dir)
-			index = node.index
-			i     = bytes.IndexByte(index, b)
+			dir      = path[:pos]
+			b        = byteForIdx(dir)
+			index    = node.index
+			i        = strings.IndexByte(index, b)
+			children = node.children
 		)
-		for ; i >= 0; i = bytes.IndexByte(index, b) {
-			if next := &node.child[i]; next.dir == dir {
+		for ; i >= 0; i = strings.IndexByte(index, b) {
+			if next := &children[i]; next.dir == dir {
 				node = next
 				continue OUTER
 			}
-			index = index[i+1:]
+			index, children = index[i+1:], children[i+1:]
 		}
 
-		return node.v, false
+		return nil
 	}
 
-	return node.v, true
+	return node
+}
+
+func (node *Node) String() string {
+	return fmt.Sprintf(
+		"Node{dir: %q, #child: %d, index: %s, type: %s}",
+		node.dir, len(node.children), string(node.index), node.typ,
+	)
 }
--- internal/radix/tree_test.go
+++ internal/radix/tree_test.go
@@ -2,37 +2,53 @@ package radix
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 )
+
+type I int
+
+func (I) ServeHTTP(_ http.ResponseWriter, _ *http.Request) {}
+
+func (t *Tree) _add(path string, v http.Handler) (ov http.Handler, replace bool) {
+	old, replace := t.Add(path, Data{v})
+	return old.Handler, replace
+}
 
 func TestTree_Add(t *testing.T) {
 	type args struct {
 		path string
-		v    int
+		v    http.Handler
 	}
 	tests := []struct {
 		args        args
-		wantOv      int
+		wantOv      http.Handler
 		wantReplace bool
 	}{
-		{args{"", 0}, None, false},
-		{args{"/", 1}, None, false},
-		{args{"/pkg", 2}, None, false},
-		{args{"/pkg/", 3}, None, false},
-		{args{"/pkg/net", 4}, None, false},
-		{args{"/doc/", 5}, None, false},
-		{args{"/pkg/net/http/httputil", 6}, None, false},
-		{args{"/pkg/net/http", 7}, None, false},
-		{args{"/pkg/net/http", 8}, 7, true},
-		{args{"/pkg/", 9}, 3, true},
-		{args{"/pkg", 10}, 2, true},
-		{args{"/", 11}, 1, true},
-		{args{"", 12}, 0, true},
+		{args{"", I(0)}, nil, false},
+		{args{"/", I(1)}, nil, false},
+		{args{"/pkg", I(2)}, nil, false},
+		{args{"/pkg/", I(3)}, nil, false},
+		{args{"/pkg/net", I(4)}, nil, false},
+		{args{"/doc/", I(5)}, nil, false},
+		{args{"/pkg/net/http/httputil", I(6)}, nil, false},
+		{args{"/pkg/net/http", I(7)}, nil, false},
+		{args{"/pkg/net/http", I(8)}, I(7), true},
+		{args{"/pkg/", I(9)}, I(3), true},
+		{args{"/pkg", I(10)}, I(2), true},
+		{args{"/", I(11)}, I(1), true},
+		{args{"", I(12)}, I(0), true},
+		{args{"/pkg/net/html", I(13)}, nil, false},
+		{args{"/pkg/net/http/httptest", I(14)}, nil, false},
+		{args{"/pkg/nnn", I(15)}, nil, false},
+		{args{"/pkg/nnnn", I(16)}, nil, false},
+		{args{"/pkg/nn", I(17)}, nil, false},
+		{args{"/pkg/nnn", I(18)}, I(15), true},
 	}
 	tree := &Tree{}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%q=%d", tt.args.path, tt.args.v), func(t *testing.T) {
-			gotOv, gotReplace := tree.Add(tt.args.path, tt.args.v)
+			gotOv, gotReplace := tree._add(tt.args.path, tt.args.v)
 			if gotOv != tt.wantOv {
 				t.Errorf("Tree.Add() gotOv = %v, want %v", gotOv, tt.wantOv)
 			}
@@ -43,42 +59,60 @@ func TestTree_Add(t *testing.T) {
 	}
 }
 
+func (t *Tree) _lookup(path string) (v http.Handler, ok bool) {
+	node := t.Lookup(path)
+	if node == nil || !node.typ.IsNotNil() {
+		return nil, false
+	}
+	return node.Handler, true
+}
+
 func TestTree_Lookup(t *testing.T) {
 	paths := []struct {
 		path string
-		v    int
+		v    http.Handler
 	}{
-		{"", 0},
-		{"/", 1},
-		{"/pkg", 2},
-		{"/pkg/", 3},
-		{"/pkg/net", 4},
-		{"/doc/", 5},
-		{"/pkg/net/http/httputil", 6},
-		{"/pkg/net/http", 7},
+		{"", I(0)},
+		{"/", I(1)},
+		{"/pkg", I(2)},
+		{"/pkg/", I(3)},
+		{"/pkg/net", I(4)},
+		{"/doc/", I(5)},
+		{"/pkg/net/http/httputil", I(6)},
+		{"/pkg/net/http", I(7)},
+		{"/pkg/net/html", I(12)},
+		{"/pkg/net/http/httptest", I(13)},
+		{"/pkg/nnn", I(14)},
+		{"/pkg/nnnn", I(15)},
+		{"/pkg/nn", I(16)},
 	}
 	tree := &Tree{}
 	for _, pp := range paths {
-		tree.Add(pp.path, pp.v)
+		tree._add(pp.path, pp.v)
 	}
 
 	tests := []struct {
 		path      string
-		wantV     int
+		wantV     http.Handler
 		wantExact bool
 	}{
-		{"", 0, true},
-		{"/", 1, true},
-		{"/pkg", 2, true},
-		{"/pkg/", 3, true},
-		{"/pkg/net", 4, true},
-		{"/doc/", 5, true},
-		{"/pkg/net/http/httputil", 6, true},
-		{"/pkg/net/http", 7, true},
+		{"", I(0), true},
+		{"/", I(1), true},
+		{"/pkg", I(2), true},
+		{"/pkg/", I(3), true},
+		{"/pkg/net", I(4), true},
+		{"/doc/", I(5), true},
+		{"/pkg/net/http/httputil", I(6), true},
+		{"/pkg/net/http", I(7), true},
+		{"/pkg/net/html", I(12), true},
+		{"/pkg/net/http/httptest", I(13), true},
+		{"/pkg/nnn", I(14), true},
+		{"/pkg/nnnn", I(15), true},
+		{"/pkg/nn", I(16), true},
 	}
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
-			gotV, gotExact := tree.Lookup(tt.path)
+			gotV, gotExact := tree._lookup(tt.path)
 			if gotV != tt.wantV {
 				t.Errorf("Tree.Lookup() gotV = %v, want %v", gotV, tt.wantV)
 			}
